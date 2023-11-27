b28afbba0975ac09641b93706d8c74f2a2509145
Roger-Melo <rogerw592@gmail.com>
2023-11-27 20:09:58 -0300

    ✅ StrictMode removido (menos chamadas para a API)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { App } from "./app.jsx"
-import "./index.css"
+import { createRoot } from 'react-dom/client'
+import { App } from './app.jsx'
+import './index.css'
 
 const rootElement = document.querySelector('[data-js="root"]')
 
 const root = createRoot(rootElement)
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+root.render(<App />)
