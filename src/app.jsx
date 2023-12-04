fe5d42aecf410b06de95a2fbe87c42f2c4abfc8f
Roger-Melo <rogerw592@gmail.com>
2023-12-04 20:35:03 -0300

    ✅ Persistência de filmes assistidos no storage do browser

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import localforage from 'localforage'
 import { useEffect, useState, useRef } from 'react'
 
 const getTotalMinutes = watchedMovies => watchedMovies
@@ -129,6 +130,22 @@ const MovieDetails = ({ clickedMovie, onClickBtnBack, onSubmitRating }) => (
 
 const useWatchedMovies = () => {
   const [watchedMovies, setWatchedMovies] = useState([])
+
+  useEffect(() => {
+    localforage.setItem('meAvalia', watchedMovies)
+      .catch(error => alert(error.message))
+  }, [watchedMovies])
+
+  useEffect(() => {
+    localforage.getItem('meAvalia')
+      .then(value => {
+        if (value) {
+          setWatchedMovies(value)
+        }
+      })
+      .catch(error => alert(error.message))
+  }, [])
+
   const handleClickBtnDelete = id => setWatchedMovies(prev => prev.filter(p => p.id !== id))
   return { watchedMovies, setWatchedMovies, handleClickBtnDelete }
 }
