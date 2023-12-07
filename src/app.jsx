7e42242925b4c6be9ded71a16c0fdb9b008dd897
Roger-Melo <rogerw592@gmail.com>
2023-12-06 21:22:17 -0300

    ✅ Criado arquivo componente MovieDetails

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,15 @@
 import { useEffect, useState } from 'react'
-import { StarRating } from './components/star-rating'
 import { History } from './components/history'
 import { NavBar } from './components/nav-bar'
 import { Movies } from './components/movies'
 import { WatchedMovies } from './components/watched-movies'
+import { MovieDetails } from './components/movie-details'
 import { useWatchedMovies } from './hooks/use-watched-movies'
-import { getMoviePoster } from './utils/get-movie-poster'
 
 const apiKey = import.meta.env.VITE_API_KEY
 const baseUrl = `https://www.omdbapi.com/?apikey=${apiKey}`
 
 const ListBox = ({ children }) => <div className="box">{children}</div>
-
-const MovieDetails = ({ clickedMovie, onClickBtnBack, onSubmitRating }) => {
-  const [rating, setRating] = useState(0)
-
-  const handleRating = userRating => setRating(userRating)
-
-  return (
-    <div className="details">
-      <header>
-        <button className="btn-back" onClick={onClickBtnBack}>&larr;</button>
-        <img src={getMoviePoster(clickedMovie.poster)} alt={`Poster de ${clickedMovie.title}`} />
-        <div className="details-overview">
-          <h2>{clickedMovie.title}</h2>
-          <p>{clickedMovie.released} &bull; {clickedMovie.runtime}</p>
-          <p>{clickedMovie.genre}</p>
-          <p><span>⭐️</span>{clickedMovie.imdbRating} IMDb rating</p>
-        </div>
-      </header>
-      <section>
-        <div className="rating">
-          <StarRating maxRating={10} size={26} color="#FCC419" onRating={handleRating} />
-          <button className="btn-add" onClick={() => onSubmitRating(rating)}>
-            + Adicionar à lista
-          </button>
-        </div>
-        <p><em>{clickedMovie.plot}</em></p>
-        <p>Elenco: {clickedMovie.actors}</p>
-        <p>Direção: {clickedMovie.director}</p>
-      </section>
-    </div>
-  )
-}
 
 const useClickedMovie = setWatchedMovies => {
   const [clickedMovie, setClickedMovie] = useState(null)
