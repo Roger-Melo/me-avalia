59d490eb15058ad5b297dfca21714a4ea2c52f39
Roger-Melo <rogerw592@gmail.com>
2023-12-08 20:27:10 -0300

    ✅ Aspas padronizadas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { resolve } from 'node:path'
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
