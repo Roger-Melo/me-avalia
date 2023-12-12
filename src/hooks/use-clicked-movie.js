59d32c41532cb1f2fc1ee0bca34ae04c358af75c
Roger-Melo <rogerw592@gmail.com>
2023-12-11 21:16:27 -0300

    ✅ Refactoring: useReducer para buscar filme clicado

--- src/hooks/use-clicked-movie.js
+++ src/hooks/use-clicked-movie.js
@@ -1,50 +1,58 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import { baseUrl } from '@/utils/base-url'
 import { request } from '@/utils/request'
 
-const useClickedMovie = setWatchedMovies => {
-  const [clickedMovie, setClickedMovie] = useState(null)
-  const [isFetchingMovieDetails, setIsFetchingMovieDetails] = useState(false)
+const getMovie = movie => ({
+  id: movie.imdbID,
+  title: movie.Title,
+  year: movie.Year,
+  imdbRating: movie.imdbRating,
+  runtime: movie.Runtime,
+  poster: movie.Poster,
+  plot: movie.Plot,
+  actors: movie.Actors,
+  director: movie.Director,
+  released: movie.Released,
+  genre: movie.Genre
+})
 
-  const handleClickBtnBack = () => setClickedMovie(null)
+const reducer = (state, action) => ({
+  dismissed_movie_details: { ...state, clickedMovie: null },
+  set_clicked_movie: { ...state, clickedMovie: action.movie && getMovie(action.movie) },
+  init_fetch: { ...state, isFetchingMovieDetails: true },
+  ended_fetch: { ...state, isFetchingMovieDetails: false }
+})[action.type] || state
+
+const useClickedMovie = setWatchedMovies => {
+  const [state, dispatch] = useReducer(reducer, { clickedMovie: null, isFetchingMovieDetails: false })
+
+  const handleClickBtnBack = () => dispatch({ type: 'dismissed_movie_details' })
   const handleClickMovie = currentClickedMovie => {
-    const prevClickedMovie = clickedMovie
+    const prevClickedMovie = state.clickedMovie
     if (prevClickedMovie?.id === currentClickedMovie.id) {
-      setClickedMovie(null)
+      dispatch({ type: 'dismissed_movie_details' })
       return
     }
 
-    setIsFetchingMovieDetails(true)
+    dispatch({ type: 'init_fetch' })
     request({
       url: `${baseUrl}&i=${currentClickedMovie.id}`,
-      onSuccess: movie => setClickedMovie({
-        id: movie.imdbID,
-        title: movie.Title,
-        year: movie.Year,
-        imdbRating: movie.imdbRating,
-        runtime: movie.Runtime,
-        poster: movie.Poster,
-        plot: movie.Plot,
-        actors: movie.Actors,
-        director: movie.Director,
-        released: movie.Released,
-        genre: movie.Genre
-      }),
-      onFinally: () => setIsFetchingMovieDetails(false)
+      onSuccess: movie => dispatch({ type: 'set_clicked_movie', movie }),
+      onFinally: () => dispatch({ type: 'ended_fetch' })
     })
   }
 
   const handleSubmitRating = userRating => {
     setWatchedMovies(prev => {
-      const duplicatedMovie = prev.some(movie => movie.id === clickedMovie.id)
+      const duplicatedMovie = prev.some(movie => movie.id === state.clickedMovie.id)
       return duplicatedMovie
-        ? prev.map(m => m.id === clickedMovie.id ? { ...clickedMovie, userRating } : m)
-        : [...prev, { ...clickedMovie, userRating }]
+        ? prev.map(m => m.id === state.clickedMovie.id ? { ...state.clickedMovie, userRating } : m)
+        : [...prev, { ...state.clickedMovie, userRating }]
     })
-    setClickedMovie(null)
+    dispatch({ type: 'dismissed_movie_details' })
   }
 
-  return { clickedMovie, handleClickBtnBack, handleClickMovie, handleSubmitRating, isFetchingMovieDetails }
+  return { clickedMovie: state.clickedMovie, handleClickBtnBack, handleClickMovie, handleSubmitRating, isFetchingMovieDetails: state.isFetchingMovieDetails }
 }
 
 export { useClickedMovie }
