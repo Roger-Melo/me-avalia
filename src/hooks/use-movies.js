fc5da1394409c970e5496060f3d86a705e24c7df
Roger-Melo <rogerw592@gmail.com>
2023-12-24 22:10:18 -0300

    ✅ useClickedMovie recriado como useMovies. useWatchedMovies removido.

--- src/hooks/use-movies.js
+++ src/hooks/use-movies.js
@@ -1,4 +1,5 @@
-import { useReducer } from 'react'
+import { useReducer, useState, useEffect } from 'react'
+import localforage from 'localforage'
 import { useLoader } from '@/hooks/use-loader'
 import { baseUrl } from '@/utils/base-url'
 import { request } from '@/utils/request'
@@ -22,10 +23,27 @@ const reducer = (state, action) => ({
   set_clicked_movie: { ...state, clickedMovie: action.movie && getMovie(action.movie) }
 })[action.type] || state
 
-const useClickedMovie = setWatchedMovies => {
+const useMovies = () => {
   const [state, dispatch] = useReducer(reducer, { clickedMovie: null })
+  const [watchedMovies, setWatchedMovies] = useState([])
   const [isFetchingMovieDetails, setIsFetchingMovieDetails] = useLoader()
 
+  useEffect(() => {
+    localforage.setItem('meAvalia', watchedMovies)
+      .catch(error => alert(error.message))
+  }, [watchedMovies])
+
+  useEffect(() => {
+    localforage.getItem('meAvalia')
+      .then(value => {
+        if (value) {
+          setWatchedMovies(value)
+        }
+      })
+      .catch(error => alert(error.message))
+  }, [])
+
+  const handleClickBtnDelete = id => setWatchedMovies(prev => prev.filter(p => p.id !== id))
   const handleClickBtnBack = () => dispatch({ type: 'dismissed_movie_details' })
   const handleClickMovie = currentClickedMovie => {
     const prevClickedMovie = state.clickedMovie
@@ -52,7 +70,7 @@ const useClickedMovie = setWatchedMovies => {
     dispatch({ type: 'dismissed_movie_details' })
   }
 
-  return { clickedMovie: state.clickedMovie, handleClickBtnBack, handleClickMovie, handleSubmitRating, isFetchingMovieDetails }
+  return { clickedMovie: state.clickedMovie, handleClickBtnBack, handleClickMovie, handleSubmitRating, handleClickBtnDelete, isFetchingMovieDetails, watchedMovies }
 }
 
-export { useClickedMovie }
+export { useMovies }
